e91d564fd371529d995322d5ed1160dacd4962d2
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-10-24 16:44:34 -0300

    Inicialized project and repo, also added favicon and title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./images/thumbnail-col.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="flex-center">
+        <img src={logo} alt="Logo do projeto em produção" />
+      </div>
     </div>
   );
 }
