da0d8e32c00e49c896a7344a171674e537dad003
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-10-25 20:04:58 -0300

    access side menu with list of places to access in the project, added functionality as well.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,25 @@
 // CSS
 import styles from "./Header.module.css";
 
+// Hooks
+import { useState } from "react";
+
 // Images
 import logo from "../../images/logo.svg";
 import bars from "../../images/bars-solid.svg";
+import close from "../../images/xmark-solid.svg";
 
 // Router
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Header = () => {
+
+  const [menu, Setmenu] = useState(false);
+
+  const handleClick = () =>{
+    Setmenu(!menu);
+  }
+
   return (
     <div className='container'>
         <nav>
@@ -20,19 +31,49 @@ const Header = () => {
             </Link>
 
             <div className="flex">
-            <li>
-              <button>
-                <Link>
-                  Baixe
-                </Link>
-              </button>
-            </li>
+              <li>
+                <button>
+                  <Link>
+                    Baixe
+                  </Link>
+                </button>
+              </li>
 
-            <li>
-              <button>
-                Menu <img src={bars} alt="menu hamburguer" />
-              </button>
-            </li>
+              <li>
+                <button onClick={handleClick}>
+                  Menu <img src={bars} alt="menu hamburguer" />
+                </button>
+              </li>
+            </div>
+
+            <div className={styles.menu} id={menu ? "inside" : null}>
+
+              <div className={styles.menu_container}>
+
+                <div className={styles.closed}>
+                  <button onClick={handleClick}>Fechar <img src={close} alt="x-mark para fechar" /></button>
+                </div>
+
+                <ul className={styles.menu_list}>
+                  <li>
+                    <Link>Home</Link>
+                  </li>
+                  <li>
+                    <Link>Grupos</Link>
+                  </li>
+                  <li>
+                    <Link>Jogos</Link>
+                  </li>
+                  <li>
+                    <Link>Baixar imagem</Link>
+                  </li>
+                  <li>
+                    <Link>FAQ</Link>
+                  </li>
+                </ul>
+
+              </div>
+
             </div>
 
           </ul>
