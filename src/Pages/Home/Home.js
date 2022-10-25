ac9cd9c7e0265515e82d63f4c1cc592052588e60
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-10-25 18:14:13 -0300

    main page organized and stylized, image for banner and its organization were added.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,39 @@
-import { Link } from "react-router-dom"
-import Header from "../../Components/Header/Header"
+// CSS
+import styles from "./Home.module.css";
+
+// Images
+import title from "../../images/title.svg";
+
+// Components
+import Header from "../../Components/Header/Header";
 
 export const Home = () => {
   return (
+  <>
     <div className="container">
       <Header></Header>
-
-      <div className="flex-center">
-        <p>Home <Link to="/teste">Teste</Link></p>
-      </div>
-
     </div>
+    
+    <div className={styles.banner}>
+      <div className="container">
+        <div className={styles.home_container}>
+          <img src={title} alt="logo da página" />
+
+          <div>
+            <p className={styles.host}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae fugit, reiciendis pariatur deserunt ducimus nesciunt officia soluta ea consectetur, accusamus repudiandae laborum dolorum. Voluptates ipsa minima quod molestiae nostrum blanditiis?
+            </p>
+            <div className="flex">
+            <button className={styles.groups}>
+              vá
+            </button>
+            <p>Grupos</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </>
   )
 }
